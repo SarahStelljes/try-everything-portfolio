ab0a35dcd0f100ab7d087b95f4ceff49731d504a
Battlehart <sarah.stelljes@gmail.com>
2022-10-01 18:50:09 -0400

    initial project

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header(){
+
+    return(
+        <header>
+            <h1>Sarah Stelljes</h1>
+            <nav>
+                {/* About Me */}
+                {/* Portfolio */}
+                {/* Contact */}
+                {/* Resume */}
+            </nav>
+        </header>
+    )
+}
